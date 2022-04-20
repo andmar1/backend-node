881f1a749be822cd90ec1e64e4797f557ad1323b
Antonio Andrade <antoniomaresmares001gmail.com>
2022-04-20 00:30:13 -0500

    peticion get en servidor

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-n
+const express = require('express');
+
+// Crear el servidor aplicacion de express 
+
+const app = express();
+
+//GET 
+app.get('/',( req, res )=>{
+
+    res.json({
+        ok:true,
+        msg:'Todo salio bien',
+        uid:1244
+    })
+
+})
+
+app.listen( 4000, ()=>{
+    console.log(`Servidor corriendo en puerto ${4000}`)
+});
